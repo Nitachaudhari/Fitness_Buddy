ae9c65d21bfb035386d23e6c8b10479b932b9563
Tharun075 <tharunrt570@gmail.com>
2025-02-06 16:54:29 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import WeeklyProgressReport from './Component/Progress_Report/ProgressReport'
+
 import WorkoutTracking from './Component/Workout_Tracking/WorkoutTracking'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <WorkoutTracking/>
-      <WeeklyProgressReport/>
+      
       
     </>
   )
